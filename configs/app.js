fe8e85a77070e21977c48498e9914bed2dd2d142
Mark <mark.sanjuan.sole@gmail.com>
2021-08-27 13:17:40 +0200

    added cors

--- configs/app.js
+++ configs/app.js
@@ -26,10 +26,13 @@ app.set('port', process.env.PORT || 3000)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(passport.initialize())
-app.use(cors({
-    origin: [ 'http://localhost:3000', 'https://acs-back.vercel.app', 'http://localhost:3001']
-}))
 app.use(cors())
+app.use(cors({
+    origin: [ 'http://localhost:3000', 'https://acs-back.vercel.app', 'http://localhost:3001'],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+
+}))
 
 // Routes
 app.use('/', index)
