59193bde1390c757940f2000324fa82512c52a1a
Mark <mark.sanjuan.sole@gmail.com>
2021-09-10 03:59:19 +0200

    added tecnologias to candidato update

--- controllers/candidato.controller.js
+++ controllers/candidato.controller.js
@@ -4,11 +4,11 @@ export const updateCandidato = async (req, res, next) => {
     const { id } = req.params
     const { dni, nombre, apellido1, apellido2, email, nacimiento, sexo, pais, provincia, direccion,
     empresaActual, puestoActual, fechaContacto, puntuacionACS, fechaPuntuacion, comentariosCandidato,
-cpostal, telefono, movil } = req.body
+cpostal, telefono, movil, tecnologias } = req.body
     const candidato = {
         dni, nombre, apellido1, apellido2, email, nacimiento, sexo, pais, provincia, direccion,
         empresaActual, puestoActual, fechaContacto, puntuacionACS, fechaPuntuacion, comentariosCandidato,
-        cpostal, telefono, movil
+        cpostal, telefono, movil, tecnologias
     }
     try {
         await Candidato.findByIdAndUpdate({ _id: id }, candidato)
